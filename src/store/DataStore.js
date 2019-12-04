ab6a1359a0cacf6e242e20f5c060fc8ecb8c19d1
François Laplaud <elfrancesco+github@gmail.com>
2019-12-04 11:33:27 +0100

    temp mobx branch

---
+++ src/store/DataStore.js
@@ -0,0 +1,12 @@
+import { observable } from "mobx"
+
+
+class DataStore {
+    constructor(rootStore) {
+        this.rootStore = rootStore;
+        
+    }
+}
+
+
+export default DataStore;
---
+++ src/store/UiState.js
@@ -0,0 +1,10 @@
+
+
+class UiState {
+    constructor(rootStore) {
+        this.rootStore = rootStore;
+    }
+}
+
+
+export default UiState;
