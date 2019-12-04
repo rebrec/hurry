1e2755b1109f26d2355ebf616340bf7d8d3e4a75
rebrec <elfrancesc.o+npm@gmail.com>
2019-12-04 00:00:00 +0000

    temp mobx branch

--- src/ui/index.js
+++ src/ui/index.js
@@ -1,8 +1,13 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 import App from "./app";
+import RootStore from "../store/RootStore"
 import "./index.css";
 import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 
-// alert('test');
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootStore = new RootStore();
+ReactDOM.render(
+    <Provider rootStore = {rootStore}>
+        <App />, document.getElementById("root")
+    </Provider>
+);
