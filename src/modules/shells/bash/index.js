814396e42c56e3f04ec1e1483019806db6acba2d
rebrec <elfrancesco@gmail.com>
2020-01-15 00:17:28 +0100

    filme removal + addes stderr output to debug console

---
+++ src/modules/shells/bash/index.js
@@ -0,0 +1,9 @@
+const processCommand = 'bash';
+const processArgs = [];
+
+module.exports = {
+    name: "bash",
+    platform: ['linux'],
+    processCommand: processCommand,
+    processArgs:  processArgs,
+}
