d4d67a22c943e3cbc7da4d7ab731f82333b5e55b
rebrec <elfrancesc.o+npm@gmail.com>
2020-04-20 00:00:00 +0000

    merging to master,going back to menu editor

--- src/example/config.js
+++ src/example/config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const app = require('electron').remote.app
 const isProd = process.env.NODE_ENV === 'production';
 const basepath = isProd ? path.join(app.getAppPath(), '.webpack', 'renderer') : path.join(__dirname, '..');
+console.log('isProd : ' + isProd);
 console.log('BASE PATH = ' + basepath);
 
 module.exports = {
