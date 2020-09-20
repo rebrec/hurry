d60d245ccfcf8a6da6733bac36cc29df04c3f66c
rebrec <elfrancesco@gmail.com>
2020-09-21 00:34:53 +0200

    feat: Add plugin context so that plugins can know their location (eg: to import a ps1 file)

--- plugins/test-plugin/index.js
+++ plugins/test-plugin/index.js
@@ -1,12 +1,15 @@
 import Plugin from '../../src/core/Plugin'
 import Test from './Test.jsx'
 import psinit from './scripts/init1.ps1'
+import datasourceDefinition from './datasource.definition'
 
 
 export default class PluginBoilerplate extends Plugin{
-    constructor(api){
-        super(api);
+    constructor(api, context ){
+        super(api); 
         api.store.uiState.addView('Test', Test(api)); 
         api.menu.insertMenuItem('Help',{label: "&Test", click: () => api.store.uiState.setCurrentView('Test')})
+        console.log('=)))))))))))))) plugin context ', context);
+        api.store.datasourceManager.addDatasourceDefinition(datasourceDefinition, context.pluginDir);
     }
 } 
