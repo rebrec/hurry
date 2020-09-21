f85aad7ffbc34b54da286d5223300755ea7f67a7
rebrec <elfrancesc.o+npm@gmail.com>
2020-09-21 00:00:00 +0000

    Finish renaming plugins to plugins-src / plugins-dist

--- plugins-src/test-plugin/scripts/init.ps1
+++ plugins-src/test-plugin/scripts/init.ps1
@@ -2,19 +2,19 @@
 class TestSearcher
 {
     hidden [String] $ClassFilePath
-    hidden [Array] $DummyData
+    hidden [Array] $db
 
     # Constructor
     TestSearcher(){
         $this.ClassFilePath = $PSScriptRoot
 
         $DataFilePath = Join-Path -Path "$($this.ClassFilePath)" -ChildPath "data.json"
-        $this.data = Get-Content $DataFilePath | ConvertFrom-JSON
+        $this.db = Get-Content $DataFilePath | ConvertFrom-JSON
     }
 
     [Array] Search([String] $Keyword){
         [Array] $res = @()
-        $this.data | % {
+        $this.db | % {
             if ($_.username -match $Keyword){
                 $res += , $_
             } elseif ($_.hostname -match $Keyword){
