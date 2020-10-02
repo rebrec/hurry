76790bfa287c503b8d5355107c5acbcfa960e6da
rebrec <elfrancesco@gmail.com>
2020-10-02 15:02:08 +0200

    refactor: remove plugins from repo

--- plugins-src/network-actions/index.js
+++
@@ -1,9 +0,0 @@
-import Plugin from '../../src/core/Plugin'
-import contextMenu from './menu'
-
-export default class TestPlugin extends Plugin{
-    constructor(api, context ){
-        super(api); 
-        api.store.registerContextMenu(contextMenu);
-    }
-} 
