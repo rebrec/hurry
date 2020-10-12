8ae92477a088ea76ba5186036290780f5876bb01
rebrec <elfrancesc.o+npm@gmail.com>
2020-10-12 10:27:24 +0200

    Improve UI design
    - Add information about installation progress
    - Improved view with search bar, plugin list and plugin detail

--- src/builtins/plugin-installer/dist/index.js
+++ src/builtins/plugin-installer/dist/index.js
@@ -1,12 +1,13 @@
 import Plugin from '../../../src/core/Plugin'
 // import Plugin from 'hurry-plugin-definition'
-import PluginInstallerView from './PluginInstallerView.jsx'
+import PluginInstallerView from './views/PluginInstallerView.jsx'
 
 export default class PluginInstaller extends Plugin{
 
     constructor(api, context){
         super(api, context)
         api.store.uiState.addView('PluginInstallerView', PluginInstallerView(api));
+        setTimeout(_=>{api.store.uiState.setCurrentView("PluginInstallerView")},2000);
         api.menu.insertMenuItem('File',{label: "&Install Plugin", click: () => api.store.uiState.showModal('PluginInstallerView')}, {after: 'Settings'});
     }
 
