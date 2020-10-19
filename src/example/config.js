b31b21c8a4820eebfd968d853a514ce3d007a117
François Laplaud <elfrancesco+github@gmail.com>
2020-10-19 10:03:02 +0200

    fix: Disallow project root choice to prevent troubles when multiple app version are available on host.

--- src/example/config.js
+++ src/example/config.js
@@ -1,21 +1,9 @@
 const path = require('path');
-const app = require('electron').remote.app
-const isProd = process.env.NODE_ENV === 'production';
-let basepath;
-if (isProd) {
-    basepath = path.join(app.getAppPath(), '.webpack', 'renderer') 
- } else {
-    basepath = path.join(__dirname, '..'); // doesn't seems to work anymore.....
-    basepath = __dirname.split('node_modules')[0] + 'src';
- } 
-console.log('isProd : ' + isProd);
-console.log('BASE PATH = ' + basepath);
 
 module.exports = {
-    projectRoot: basepath,
     menu: {
         menuPath: path.join(__dirname, "..", "modules", "menuConfig.json"),
-        submenuShowOnClick: false
+        submenuShowOnClick: true
     },
     datasources: {
         glpi: {
