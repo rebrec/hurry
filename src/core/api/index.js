8c9ec1099a6c125b209158fb6f1a1a8c5a7f3253
Francois Laplaud <francois.laplaud@lemans.fr>
2022-06-13 22:37:12 +0200

    Feat Expose host clipboard through API

--- src/core/api/index.js
+++ src/core/api/index.js
@@ -1,7 +1,7 @@
 import { config } from '../../config'
 import store from '../../store/RootStore'
 import MenuManager from '../../core/MenuManager'
-import {remote} from "electron"
+import {remote, clipboard} from "electron"
 import Logger from '../helpers/logging';
 const logger = Logger('Api');
 const { dialog } = remote
@@ -10,6 +10,10 @@ class Api {
     constructor() {
         this.config = config;
         this.store = store;
+        this.electron = {
+            remote: remote,
+            clipboard: clipboard
+        }
         this.helpers = {
             Logger: Logger
         }
